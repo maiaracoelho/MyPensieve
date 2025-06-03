a8494aff5fd4c48b6203a58a119b9c9577228af6
May <maiara.coelho@gruposbf.com.br>
2025-06-03 09:01:28 -0400

    remove build

--- run_on_remote.sh
+++ run_on_remote.sh
@@ -5,7 +5,7 @@ START=$(date +%s)
 LOG_FILE=exec_$(date +%Y%m%d_%H%M%S).log
 
 # Executa docker-compose em segundo plano com nohup
-nohup /usr/bin/time -v docker-compose --compatibility up --build > $LOG_FILE 2>&1 &
+nohup /usr/bin/time -v docker-compose --compatibility up > $LOG_FILE 2>&1 &
 
 # Guarda o PID
 echo "🟢 Docker rodando em segundo plano com PID $!"
